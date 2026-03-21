679df843b095d402a4994838fe8999d6feb816ff
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2026-03-21 15:18:02 +0300

    +

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IRS-1_
 IRS-1 - Intelligent (mobile) robotic systems
+
+# arduinoDriver
+ros2 run arduinoDriver arduinoDriver_node.py
+ros2 run arduinoDriver arduinoDriver_node.py --ros-args -p port:=/dev/ttyUSB0 -p rate:=20.0 --remap motor_speed_left:=/left_wheel_speed
--- arduinoDriver/setup.py
+++ arduinoDriver/setup.py
@@ -17,13 +17,14 @@ setup(
     maintainer_email='53381511+Ni3nayka@users.noreply.github.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    # extras_require={ # МУСОР
+    #     'test': [
+    #         'pytest',
+    #     ],
+    # },
     entry_points={
         'console_scripts': [
+            'arduinoDriver = arduinoDriver.arduinoDriver_node:main', # НОДА
         ],
     },
 )
