adb1a18bccc3f1d4dc5a1de861023b97740b186e
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2026-03-21 16:07:31 +0300

    clear

--- build/arduinoDriver/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ni3nayka/Документы/GitHub/IRS-1_sber_hack_2026/install/arduinoDriver'
